44215b2a1f9df656ad2d180c580bee6143bc5e95
Fatimah <Nyzak1@outlook.sa>
2023-12-12 21:39:23 +0300

    after ISP

--- bin/isp/after_isp.dart
+++ bin/isp/after_isp.dart
@@ -1,28 +1,51 @@
 //forth principal ISP means classes that implement interfaces should not be forced to implement methods that they do not need
 //after ISP
-abstract class IPayment{
-  void payWithGooglePay();
-  void payWithCreditCard();
+abstract class IPayment {
+  // void payWithGooglePay();
+  // void payWithCreditCard();
 }
 
-abstract class IIosPayement{
+class payment implements IIosPayement, ICreditCardPayement{
+  @override
+  void payCreditCard() {
+    // TODO: implement payCreditCard
+  }
+
+  @override
+  void payWithApplePay() {
+    // TODO: implement payWithApplePay
+  }
+
+}
+
+abstract class IIosPayement {
   void payWithApplePay();
 }
 
-abstract class IAndroidPayement{
+abstract class IAndroidPayement {
   void payWithGooglePay();
 }
 
-abstract class ICreditCardPayement{
+abstract class ICreditCardPayement {
   void payCreditCard();
 }
 
-class IosPayement implements IIosPayement{
+class IosPayement implements IIosPayement {
   @override
   void payWithApplePay() {
-  }}
+    // TODO: implement payWithApplePay
+  }
+}
 
-class AndroidPayment implements IAndroidPayement{
+class AndroidPayment implements IAndroidPayement {
   @override
-  void payWithGooglePay() {
-  }}
+  void payWithGooglePay() {}
+  // TODO: implement payWithGooglePay
+}
+
+class CreditCardPayement implements ICreditCardPayement {
+  @override
+  void payCreditCard() {
+    // TODO: implement payCreditCard
+  }
+}
